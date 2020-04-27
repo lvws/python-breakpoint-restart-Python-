99dd4c016207a53f60cd0fe3066e256a1ce96aca
lvws <1129309258@qq.com>
2020-04-27 17:45:00 +0800

    装饰器，运行时，便能记录运行状态

--- pointRestart.py
+++ pointRestart.py
@@ -4,24 +4,32 @@ Created on Sun Apr 26 15:59:55 2020
 
 @author: Administrator
 """
-import time,types
+import time,types,pickle,sys
 
-
+def storeTmp():
+    keys = globals().keys() # 拿到目前所有的变量（因为是动态的所以）
+    filterTp = [types.FunctionType,types.ClassType,types.ModuleType]
+    dic = {}
+    for key in keys:
+        if type(globals()[key]) in filterTp:
+            continue
+        dic[key] = globals()[key]
+    print dic
+    with open(sys.argv[0]+'.pkl','wb') as f:
+        pickle.dump(dic,f)
 
 def logFunc(func):
     def run(*args,**kargs):
         nRunlst.append(func.__name__) # 记录运行的func
-        if p.get("aRunlst"):          # 如果上次运行过了，就不做了
-            p.get("aRunlst").remove(func.__name__)
+        if globals().get("aRunlst"):          # 如果上次运行过了，就不做了
+            globals().get("aRunlst").remove(func.__name__)
             print "Pass"
             return 
         now = time.time()
         print "Now Running: " + func.__name__
-#        print "args:",args,kargs
         func(*args,**kargs)
         print "function over,use time: " + str((time.time() - now))
-        print p['age']    
-    
+        storeTmp()
     return run
 
 @logFunc
@@ -40,9 +48,11 @@ class Person():
     def __init__(self,name,age):
         self.name = name
         self.age = age
+        
 
 
-p = locals() # 动态记录所有的变量
+
+ # 动态记录所有的变量
 nRunlst = [] # 记录已经运行的func
 aRunlst = ['printf','add','printf'] # 模拟上一次运行过的func 列表
 
@@ -59,12 +69,4 @@ printf("good",[1,2,3,4],"jj",'dog')
 add(20)
 
 print nRunlst 
-
-keys = p.keys() # 拿到目前所有的变量（因为是动态的所以）
-
-filterTp = [types.FunctionType,types.ClassType,types.ModuleType]
-for key in keys:
-    if type(p[key]) in filterTp:
-        continue
-    print key,p[key]
 
